013bab0643b93a8bf689778735dda226c65dddfc
William-Abbot <59292592+William-Abbot@users.noreply.github.com>
2022-09-05 14:28:02 -0500

    src and public files + gitignore

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello, Welcome To my website!
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/William-Abbot"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
